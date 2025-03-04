a5dd19ed1489f7b2f5f6fbaf354c81fae9cb0ea6
wesleybentura13 <wesleybentura@gmail.com>
2025-03-04 11:02:28 +0200

    New Login Page - with fix

--- Code/login.ts
+++ Code/login.ts
@@ -1,9 +1,9 @@
 app.post("/register", (req, res) => {
     const { username, password, role } = req.body; // Role is meant to be "user" or "admin"
   
-    const query = `INSERT INTO users (username, password, role) VALUES ('${username}', '${password}', '${role}')`;
+    const query = "INSERT INTO users (username, password, role) VALUES (?,?,?)";
   
-    db.query(query, (err, results) => {
+    db.query(query, [username, password, role], (err, results) => {
       if (err) {
         console.error("Database error:", err);
         return res.status(500).json({ message: "Internal server error" });
